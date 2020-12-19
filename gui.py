8e58a5fe45b9d1c955296d50fc4409466e267a91
Piyush Gulwani <dheergulwani1@gmail.com>
2020-12-19 14:39:32 +0530

    UnderDevlopment

---
+++ gui.py
@@ -0,0 +1,14 @@
+
+#! Importing Modules
+from tkinter import *
+import tkinter as tk
+from PIL import Image 
+
+#! Basic Configurations
+gui = Tk()
+gui.geometry('500x700')
+gui.configure(bg = 'black')
+
+#! Taking Image 
+
+gui.mainloop()
