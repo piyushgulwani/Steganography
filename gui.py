fcf870efa48569c666d2660ff9b7f2be8118d0b3
Piyush Gulwani <dheergulwani1@gmail.com>
2020-12-19 14:51:06 +0530

    Added Taking Image

--- gui.py
+++ gui.py
@@ -6,9 +6,16 @@ from PIL import Image
 
 #! Basic Configurations
 gui = Tk()
+gui.title('Image Steganography')
 gui.geometry('500x700')
 gui.configure(bg = 'black')
 
 #! Taking Image 
+f1 = Frame(gui)
+e1_value = StringVar()
+Label(f1, text = 'Enter Image Path ➡').pack(side = LEFT)
+e1 = Entry(f1, textvariable = e1_value, font = '10')
+e1.pack()
+f1.pack(side = TOP, pady = 30)
 
 gui.mainloop()
